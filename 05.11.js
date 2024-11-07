2a7b9060122d949b8a7b1190644a8e7b727fb87b
kristina-per <pheradzekristina5@gmail.com>
2024-11-07 12:21:11 +0400

    Add files via upload

---
+++ 05.11.js
@@ -0,0 +1,18 @@
+let Age = -9; 
+
+
+if (Age <= 12  && Age > 0 ) {
+    console.log ( 'ticketPrice is 5$')
+} else if (Age >12 && Age <=17) {
+    console.log ('ticketPrice is 10$')
+} else if (Age >= 18 && Age <=64 ){ 
+    console.log ('ticketPrice is 15$')
+} else if (Age >= 65 && Age <=120 ) {  
+    console.log ('ticketPrice is 8$')
+}
+ else { 
+    console.log ('Not the correct Age')
+    
+}
+
+
